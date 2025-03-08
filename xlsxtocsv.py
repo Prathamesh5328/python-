986f06b9a5f2543c1f1de781a091d4d339ec760f
Prathamesh Keote <166607040+Prathamesh5328@users.noreply.github.com>
2025-03-08 21:28:08 +0530

    Delete xlsxtocsv.py

--- xlsxtocsv.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-from openpyxl import load_workbook 
-dataset = load_workbook(filename = 'Telco_customer_churn.xlsx') 
-df = pd.DataFrame(dataset)
-# df.to_csv('Telco_customer_churn.xlsx')
-# pd.read_csv('Telco_customer_churn.xlsx') 
